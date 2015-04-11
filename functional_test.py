a970f326ed7dc9f9f3f25dd0427efc4e205c7d74
Alkalit <lyoffa412@gmail.com>
2015-04-11 12:17:51 +0500

    First real FT.

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,39 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_get_it_later(self):
+        # Пахом зашел на главную страницу
+        self.browser.get('http://localhost:8000')
+
+        # И обратил внимание на заголовок в браузере
+        self.assertIn('To-Do', self.browser.title)
+
+        # Он увидел что можно внести заметку
+
+        # Он ввел "Купить сладкого хлеба" в текстовое поле
+        # Когда он нажал ввод, страница обновилась, и на странице начался список:
+        # "1: Купить сладкого хлеба" как элемент списка.
+
+        # Окошко для ввдоа также было доступно
+        # Пахом ввел:
+        # "Накормить братишку". Пахом очень последователен
+        # Страница снова обновилася и теперь на ней было видно два элемента списка:
+
+        # Пахому стало интересно, сохранится ли список. И он заметил что сайт
+        # сгенерировал для него уникальный урл. Этому способствовал пояснительный текст
+
+        # Он перешел по этому урлу и его список дел был по прежднему там
+
+        # Удовлетворенный, Пахом пошел спать.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
