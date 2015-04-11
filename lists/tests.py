2e09e56fc9d896cdecb1267033acb58ccb8f7c7b
Alkalit <lyoffa412@gmail.com>
2015-04-11 15:31:23 +0500

    Разметка домашней страницы перенесена в отдельный шаблон.

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http.request import HttpRequest
+from django.template.loader import render_to_string
+
 from lists.views import home_page
 
 
@@ -18,4 +20,5 @@ class HomePageTest(TestCase):
         # Смотрим что нам пришла html-страница с нужным содержимым.
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do list</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        # стрип нужен из символа \n в конце файла
+        self.assertTrue(response.content.strip().endswith(b'</html>'))
