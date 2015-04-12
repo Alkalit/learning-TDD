d428c0235240efa3fd8ff067318187f5adb48e73
Alkalit <lyoffa412@gmail.com>
2015-04-12 14:30:59 +0500

    Повторяющийся код вынесен в отдельный метод.

--- functional_test.py
+++ functional_test.py
@@ -29,14 +29,13 @@ class NewVisitorTest(unittest.TestCase):
 
         # Он ввел "Купить сладкого хлеба" в текстовое поле
         input_.send_keys('Купить сладкого хлеба')
+
         # Когда он нажал ввод, страница обновилась,
         input_.send_keys(Keys.ENTER)
+
         # и на странице начался список:
         # "1: Купить сладкого хлеба" как элемент списка.
-
-        table = self.browser.find_element_by_id('list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Купить сладкого хлеба', [row.text for row in rows])
+        self.assertTODOInTable('1: Купить сладкого хлеба')
 
         # Окошко для ввдоа также было доступно
         # Пахом ввел:
@@ -46,10 +45,8 @@ class NewVisitorTest(unittest.TestCase):
         input_.send_keys(Keys.ENTER)
 
         # Страница снова обновилася и теперь на ней было видно два элемента списка:
-        table = self.browser.find_element_by_id('list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Купить сладкого хлеба', [row.text for row in rows])
-        self.assertIn('2: Накормить братишку', [row.text for row in rows])
+        self.assertTODOInTable('1: Купить сладкого хлеба')
+        self.assertTODOInTable('2: Накормить братишку')
 
         # Пахому стало интересно, сохранится ли список. И он заметил что сайт
         # сгенерировал для него уникальный урл. Этому способствовал пояснительный текст
@@ -57,6 +54,17 @@ class NewVisitorTest(unittest.TestCase):
         # Он перешел по этому урлу и его список дел был по прежднему там
 
         # Удовлетворенный, Пахом пошел спать.
+
+    def assertTODOInTable(self, to_list_element):
+        '''
+        В книге обозначено как check_for_row_in_table
+        Проверяет наличие элементов списка в таблице.
+        Вынесено в отдельный метод из-за частого использования этого сниппета..
+        '''
+
+        table = self.browser.find_element_by_id('list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(to_list_element, [row.text for row in rows])
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
