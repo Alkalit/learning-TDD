2e7c0b72dc45b3cb2fa07e65c078abe290aec527
Alkalit <lyoffa412@gmail.com>
2015-04-12 15:53:20 +0500

    Домашняя вьюха возвращает редирект после post

--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http.response import HttpResponse
 
 from lists.models import Item
 
 
 def home_page(request):
-    item = Item()
-    item.text = request.POST.get('item_text', '')
-    item.save()
 
-    return render(request, 'lists/home.html', {
-        'new_item_text': item.text
-    })
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    return render(request, 'lists/home.html')
