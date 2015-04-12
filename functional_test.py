9db18f48f4e39366ccbff87e0dd564f485b7e87b
Alkalit <lyoffa412@gmail.com>
2015-04-12 14:17:47 +0500

    Добавленна дополнительная проверка сохраняются предыдущие введенные элементы списка.

--- functional_test.py
+++ functional_test.py
@@ -41,7 +41,15 @@ class NewVisitorTest(unittest.TestCase):
         # Окошко для ввдоа также было доступно
         # Пахом ввел:
         # "Накормить братишку". Пахом очень последователен
+        input_ = self.browser.find_element_by_id('new_item')
+        input_.send_keys('Накормить братишку')
+        input_.send_keys(Keys.ENTER)
+
         # Страница снова обновилася и теперь на ней было видно два элемента списка:
+        table = self.browser.find_element_by_id('list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Купить сладкого хлеба', [row.text for row in rows])
+        self.assertIn('2: Накормить братишку', [row.text for row in rows])
 
         # Пахому стало интересно, сохранится ли список. И он заметил что сайт
         # сгенерировал для него уникальный урл. Этому способствовал пояснительный текст
