f07f5535b8dc9bf5b4dd2d47b93434c04b7675ec
Alkalit <lyoffa412@gmail.com>
2015-05-09 23:26:28 +0500

    Рефакторинг: домашняя вьюха не выводит список тудушек. Т.к. это теперь делает отдельная вьюха.

--- lists/views.py
+++ lists/views.py
@@ -10,7 +10,6 @@ def home_page(request):
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/lists/the-only-list-in-the-world/')
 
-    items = Item.objects.all()
     return render(request, 'lists/home.html', {'items': items})
 
 def view_list(request):
