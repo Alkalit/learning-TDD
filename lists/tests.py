3589aa091b695f6d916822e360f16ced0f058faf
Alkalit <lyoffa412@gmail.com>
2015-05-10 16:37:56 +0500

    RED: модель List которая может содержать тудушки

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.http.request import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -22,16 +22,24 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'The second item'
+        first_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.all()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -40,7 +48,9 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
 
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'The second item')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
