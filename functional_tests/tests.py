b928a8d510f54b7cb9c133c06c73e5314491d50e
Alkalit <lyoffa412@gmail.com>
2015-05-16 15:49:02 +0500

    Добавлена возможность запускать функциональные тесты против определенного доменного имени

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,29 @@
+import sys
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls): # setUpClass исполняется единожды перед запуском тестового класса
+        '''
+        Если тесты запущены с заданным хостингом, то использовать его.
+        Иначе использовать обычную тестовую конфигурацию.
+        '''
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return # прекратить исполнение метода.
+
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -15,7 +36,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_can_start_a_list_and_get_it_later(self):
         # Пахом зашел на главную страницу
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # И обратил внимание на заголовок в браузере
         self.assertIn('To-Do', self.browser.title)
@@ -68,7 +89,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Братишка зашел на домашнюю страницу. И не увидел ни следа от
         # списка Пахома
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('1: Купить сладкого хлеба', page_text)
         self.assertNotIn('2: Накормить братишку', page_text)
@@ -87,7 +108,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Пахом зашел на домашнюю страницу
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # Он отметил что форма ввода отцентрирована - ништяк!
