c88d74d6650ba1ef79220c70f3ad0111b4a1c4ff
Alkalit <lyoffa412@gmail.com>
2015-05-17 16:12:06 +0500

    Red: тест на недопустимость ввода пустой строки.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,16 +5,28 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_item(self):
         # Пахом зашел на домашнюю страницу и случайно нажал ввод при пустом поле
         # ввода
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('new_item').send_keys('\n')
 
         # Страница обновилась и появилось сообщение об ошибке, говорящее о том
         # что тудушка не может быть пустой
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # Он попробовал ввести какой-нибудь текст для проверки и теперь он
         # добавился
+        self.browser.find_element_by_id('id_new_item').send_keys('Сочинить песню про слоника\n')
+        self.assertTODOInTable('1: Сочинить песню про слоника')
 
         # Намеренно, он теперь решил ввести пустой ввод еще раз.
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # Сообщение об ошибке появилось снова.
+        self.assertTODOInTable('1: Сочинить песню про слоника')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # И он исправил это введя новую тудушку.
-        pass
+        self.browser.find_element_by_id('id_new_item').send_keys('Приготовить сладкого хлеба\n')
+        self.assertTODOInTable('1: Сочинить песню про слоника')
+        self.assertTODOInTable('2: Приготовить сладкого хлеба')
