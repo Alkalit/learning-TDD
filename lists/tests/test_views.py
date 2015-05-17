ff9b1e6069e0e65c76c7b1def56dd458ae20d0bc
Alkalit <lyoffa412@gmail.com>
2015-05-17 16:57:54 +0500

    Red: тест на вывод сообщения об ошике в форме в случае ввода пустой тудушки.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -79,6 +79,13 @@ class ListViewTest(TestCase):
 
 class NewItemTest(TestCase):
 
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'lists/home.html')
+        expected_error = "You can't have an empty list item"
+        self.assertContains(response, expected_error)
+
     def test_can_save_POST_request_to_an_existing_list(self):
         correct_list = List.objects.create()
         other_list = List.objects.create()
