beffc2592e0531a0db16db15e526c8506775f7e2
Alkalit <lyoffa412@gmail.com>
2015-05-19 15:08:21 +0500

    Refactor: тест на валидация на домашней странице разделен на несколько меньших

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,7 +6,7 @@ from django.utils.html import escape
 
 from lists.views import home_page
 from lists.models import Item, List
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_LIST_ERROR
 
 
 class HomePageTest(TestCase):
@@ -114,14 +114,21 @@ class ListViewTest(TestCase):
 
 class NewItemTest(TestCase):
 
+    def test_for_invalid_input_renders_home_template(self):
+        response = self.client.post('/lists/new', data={'text': '', })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_validation_erros_are_shown_on_home_page(self):
+        response = self.client.post('/lists/new', data={'text': '', })
+        self.assertContains(response, EMPTY_LIST_ERROR)
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.client.post('/lists/new', data={'text': '', })
+        self.assertIsInstance(response.context['form'], ItemForm)
+
     def test_invalid_list_items_arent_saved(self):
         self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
-        response = self.client.post('/lists/new', data={'text': ''})
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'lists/home.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
