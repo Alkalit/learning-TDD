56a27e33f65e7024cccaa9371d88a36cd1aec7d0
Alkalit <lyoffa412@gmail.com>
2015-05-20 14:27:04 +0500

    Refactoring: тест на создание и сохраниние Item заменен более простой проверкой на реляцию к List

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -6,6 +6,16 @@ from lists.models import Item, List
 
 class ListAndItemModelTest(TestCase):
 
+    def test_default_text(self):
+        item = Item()
+        self.assertEqual(item.text, '')
+
+    def test_item_is_related_to_list(self):
+        list_ = List.objects.create()
+        item = Item.objects.create(list=list_)
+
+        self.assertIn(item, list_.item_set.all())
+
     def test_string_representation(self):
         item = Item(text='some text')
 
@@ -45,34 +55,6 @@ class ListAndItemModelTest(TestCase):
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % list_.id)
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'The second item'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'The second item')
-        self.assertEqual(second_saved_item.list, list_)
 
     def test_cannot_save_empty_list_item(self):
         list_ = List.objects.create()
